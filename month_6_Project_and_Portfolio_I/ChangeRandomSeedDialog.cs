974ee5c00edbc4395a7fed724d4124cb73a7256c
jaacko-torus <jaacko.torus@gmail.com>
2021-04-28 19:40:24 -0400

    finished change random seed

---
+++ month_6_Project_and_Portfolio_I/ChangeRandomSeedDialog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace month_6_Project_and_Portfolio_I {
+    public partial class ChangeRandomSeedDialog : Form {
+        private Random random = new Random();
+        public int seed {
+            get => (int)this.numericUpDownSeed.Value;
+            set => this.numericUpDownSeed.Value = value;
+        }
+
+        public ChangeRandomSeedDialog() {
+            this.InitializeComponent();
+        }
+
+        private void buttonRandomize_Click(object sender, EventArgs e) {
+            this.seed = random.Next();
+        }
+    }
+}
